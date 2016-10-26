43f01cff0555d3269a83ab659125e1c5a0ea8868
Ela Korkmaz <elakorkmaz87@gmail.com>
2016-10-26 19:25:58 +0200

    got it working

--- app.js
+++ app.js
@@ -38,22 +38,22 @@ app.get('/media', function(req, res) {
 
 app.get('/books', function(req, res) {
   console.log('Requesting /books');
-  res.render('views/index.pug', { books: bookData });
+  res.render('books.pug', { books: bookData });
 });
 
 app.get('/books/*', function(req, res) {
   var foundBook = findBook(req.params[0]);
-  res.render('views/book.pug', { book: foundBook });
+  res.render('book.pug', { book: foundBook });
 });
 
 app.get('/movies', function(req, res) {
 	console.log('Requesting /movies');
-	res.send(pug.renderFile('views/movies.pug', { movies: movieData }));
+	res.send(pug.renderFile('movies.pug', { movies: movieData }));
 });
 
 app.get('/movies/*', function(req, res) {
 	var foundMovie = findMovie(req.params[0]);
-	res.render('views/movie.pug', { movie: foundMovie });
+	res.render('movie.pug', { movie: foundMovie });
 });
 
 app.listen(3001, function() {
